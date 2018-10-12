a79ccfa68e22afa1cb5fb5f57a14e49813fb8567
EmiYot <43979441+EmiYot@users.noreply.github.com>
2018-10-12 17:50:00 +0900

    四塚恵美

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
-"""
-Spyderエディタ
+# -*- coding: utf-8 -*-
+def fib(n):
+    a,b=0,1
+    for i in range(n):
+        a,b=b,a+b
+    return b
+print([fib(i) for i in range(10)])
+    
+    
+print("フィボナッチ数列の2018番目は" + str(fib(2018)) + "です")
 
-これは一時的なスクリプトファイルです
-"""
-
-print(4*2)
+      
+        
+        
+        
+    
+    
+    
